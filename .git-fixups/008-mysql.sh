05a0cd2fac9f06d61c99ec0149cedb3a413ac976
devspone <devspone@gmail.com>
2018-07-19 13:10:16 +0000

    Fixing more mysql permissions at boot stage

--- .git-fixups/008-mysql.sh
+++ .git-fixups/008-mysql.sh
@@ -7,3 +7,6 @@ chown mysql:adm /var/log/mysql/
 chown mysql:adm /var/log/mysql/*
 chmod 755 /var/log/mysql/
 chmod 660 /var/log/mysql/*
+mkdir -vp /var/lib/mysql
+chown -R mysql:mysql /var/lib/mysql/
+chmod 700 /var/lib/mysql/
